746b7c4785861f507ae6c1139da941b087ae669b
Mauro <mauro.smg.1801@gmail.com>
2023-11-09 18:36:09 -0300

    rellenando dulce

--- src/main/java/Tarea/Dulce.java
+++ src/main/java/Tarea/Dulce.java
@@ -1,4 +1,12 @@
 package Tarea;
-
-public class Dulce {
+/** Tipo de producto que hay en el expendedor. */
+public abstract class Dulce extends Producto {
+    /** Tipo de producto que hay en el expendedor.
+     * @param serie Número para identificar el producto. */
+    public Dulce(int serie) {
+        super(serie);
+    }
+    /** lo que hace el cliente luego de recibir su dulce.
+     * @return El sabor del dulce. */
+    public abstract String comer();
 }
