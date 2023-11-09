b6dd6542006ac5c298ff995a7c1719a0691cd25b
Mauro <mauro.smg.1801@gmail.com>
2023-11-09 19:16:51 -0300

    rellenando fanta

--- src/main/java/Tarea/Fanta.java
+++ src/main/java/Tarea/Fanta.java
@@ -1,4 +1,12 @@
 package Tarea;
-
-public class Fanta {
+/** Un tipo de bebida. */
+public class Fanta extends Bebida {
+    /** Un tipo de bebida.
+     * @param serie Número para identificar el producto. */
+    public Fanta(int serie) {
+        super(serie);
+    }
+    public String beber() {
+        return "fanta";
+    }
 }
