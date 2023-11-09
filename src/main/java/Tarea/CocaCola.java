f8727d0067ce0b76b7790bf8dffb6e18b4994d51
Mauro <mauro.smg.1801@gmail.com>
2023-11-09 18:19:25 -0300

    rellenando cocacola

--- src/main/java/Tarea/CocaCola.java
+++ src/main/java/Tarea/CocaCola.java
@@ -1,4 +1,12 @@
 package Tarea;
-
-public class CocaCola {
+/** Un tipo de bebida. */
+public class CocaCola extends Bebida {
+    /** Un tipo de bebida.
+     * @param serie Número para identificar el producto. */
+    public CocaCola(int serie) {
+        super(serie);
+    }
+    public String beber() {
+        return "cocacola";
+    }
 }
