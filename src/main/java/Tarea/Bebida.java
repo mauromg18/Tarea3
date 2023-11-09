3f4e675a6522490b39e6254b9ecf363eec8e6607
Mauro <mauro.smg.1801@gmail.com>
2023-11-09 18:19:11 -0300

    rellenando bebida

--- src/main/java/Tarea/Bebida.java
+++ src/main/java/Tarea/Bebida.java
@@ -1,4 +1,12 @@
 package Tarea;
-
-public class Bebida {
+/** Tipo de producto que esta en el expendedor. */
+public abstract class Bebida extends Producto {
+    /** Tipo de producto que esta en el expendedor.
+     *  @param serie Número para identificar el producto. */
+    public Bebida(int serie) {
+        super(serie);
+    }
+    /** lo que hace el cliente luego de recibir su bebida desde el expendedor.
+     * @return El sabor de la bebida. */
+    public abstract String beber();
 }
